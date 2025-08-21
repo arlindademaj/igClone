a11b6a3141b0a7d383d253884592aff27b224bc8
arlindademaj <aarlindademaj@gmail.com>
2025-08-20 23:56:47 -1100

    colorTheme

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,29 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "./components/ui/provider";
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config, styles });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider>
+    <Provider theme={theme}>
       <App />
     </Provider>
   </StrictMode>
